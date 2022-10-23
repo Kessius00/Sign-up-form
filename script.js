87c99b0ca180d321e424dc2b400d1d065472ea35
kessius00 <kes.olling00@gmail.com>
2022-10-23 14:52:44 +0200

    Adding script.js along with script for checking if passwords match, last html changes

---
+++ script.js
@@ -0,0 +1,29 @@
+// Constants
+const password = document.getElementById('password')
+const passwordConfirm = document.getElementById('password_confirm')
+
+
+
+passwordConfirm.addEventListener('focusout', ()=>{
+    changeValidity(equalValidation(password.value, passwordConfirm.value))
+});
+
+function equalValidation(psswrd, cnfrmPsswrd){
+    return psswrd == cnfrmPsswrd;
+};
+
+function changeValidity(same){
+    if (same){
+        password.style.borderColor = 'greenyellow';
+        passwordConfirm.style.borderColor = 'greenyellow';
+        return
+    } 
+    password.style.borderColor = 'red';
+    passwordConfirm.style.borderColor = 'red';
+    alert('Passwords do not match!')
+    return 
+
+}
+
+
+
